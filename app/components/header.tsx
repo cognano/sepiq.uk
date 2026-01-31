b01499d15d68e90c0d0f6198a703422268540468
linyows <linyows@gmail.com>
2026-01-31 22:08:07 +0900

    use component instead of svg

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
 import styles from "./header.module.css";
+import SepiqLogo from "./SepiqLogo";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Image src="/sepiq.svg" width={180} height={60} alt="SEPIQ" />
+      <SepiqLogo />
     </header>
   )
-};
+}
