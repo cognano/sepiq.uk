6e1992ae917c9aff9ded486924118ce5170dc4e7
linyows <linyows@gmail.com>
2026-03-18 21:52:40 +0900

    fix lint

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -7,9 +7,15 @@ export default function Header() {
     <header className={styles.header}>
       <SepiqLogo />
       <nav className={styles.nav}>
-        <Link href="/" className={styles.navLink}>Home</Link>
-        <Link href="/about" className={styles.navLink}>About</Link>
-        <Link href="/brand" className={styles.navLink}>Brand</Link>
+        <Link href="/" className={styles.navLink}>
+          Home
+        </Link>
+        <Link href="/about" className={styles.navLink}>
+          About
+        </Link>
+        <Link href="/brand" className={styles.navLink}>
+          Brand
+        </Link>
       </nav>
     </header>
   );
