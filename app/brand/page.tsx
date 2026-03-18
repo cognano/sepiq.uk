6a45c11b0f802af953f062f7c58fb672b48117ce
linyows <linyows@gmail.com>
2026-03-18 21:51:02 +0900

    add brand page

---
+++ app/brand/page.tsx
@@ -0,0 +1,15 @@
+import Blocks from "../components/blocks";
+import { GetContents } from "../lib/contents";
+import styles from "./page.module.css";
+
+export default async function Brand() {
+  const brand = await GetContents("brand");
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.mainInner}>
+        <Blocks blocks={brand} />
+      </div>
+    </div>
+  );
+}
