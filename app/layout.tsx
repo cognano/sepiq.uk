edececbb599239b0f9dc4f1a7681f71ec81737d2
linyows <linyows@gmail.com>
2026-03-19 15:09:32 +0900

    split viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "rotion/style.css";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import FloatingCircles from "./components/FloatingCircles";
@@ -44,8 +44,12 @@ export const metadata: Metadata = {
     images: [image],
     creator: "",
   },
-  viewport: "width=device-width, initial-scale=1",
   robots: "index, follow",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
